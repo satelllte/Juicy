7d61efb1299834ff0d8b7bed62e89bff04363ac1
George Satellite <lunaerxs@gmail.com>
2023-08-07 18:19:54 +0300

    ci: test_format

--- test_format.sh
+++ test_format.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+clang-format --version
+echo "---"
+
 SOURCE_FILES=$(find "Source" -type f \( -name "*.cpp" -o -name "*.h" \))
 
-echo "Checking clang-format for files:"
+echo "Checking formatting for files:"
 echo "$SOURCE_FILES"
 
 clang-format --dry-run -style=file $SOURCE_FILES -Werror
