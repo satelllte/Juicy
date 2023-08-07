77ade204f231310a2bea6c996924d3dab974b104
George Satellite <lunaerxs@gmail.com>
2023-08-07 19:56:26 +0300

    clang-tidy WIP

---
+++ test_lint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clang-tidy --version
+echo "---"
+
+SOURCE_FILES=$(find "Source" -type f \( -name "*.cpp" -o -name "*.h" \))
+
+echo "Running linter test for files:"
+echo "$SOURCE_FILES"
+
+clang-tidy $SOURCE_FILES
