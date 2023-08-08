3d9483d32a967a474e02e142c721f4d81067a448
George Satellite <lunaerxs@gmail.com>
2023-08-08 15:59:32 +0300

    more formatting errors in the same file

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -16,7 +16,7 @@ public:
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
-    PluginProcessor& processorRef;
+ PluginProcessor& processorRef;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginEditor)
+            JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginEditor)
 };
